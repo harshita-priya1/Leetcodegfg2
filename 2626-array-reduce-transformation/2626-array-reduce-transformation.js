0b617b07f3cc9f0d13269af0ef9a7603584acf52
harshita-priya1 <88089460+harshita-priya1@users.noreply.github.com>
2023-08-12 19:10:35 +0530

    Time: 58 ms (63.37%), Space: 41.9 MB (87.22%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -5,9 +5,5 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    let val=init;
-    for(let i=0;i<nums.length;i++){
-        val = fn(val, nums[i]);
-    }
-    return val;
+      return nums.reverse().reduceRight((val, num) => fn(val, num), init);
 };
