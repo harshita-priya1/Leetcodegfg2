122c781815ac377fc4971a40cf64abf275d2daaa
harshita-priya1 <88089460+harshita-priya1@users.noreply.github.com>
2023-08-17 01:07:43 +0530

    Time: 70 ms (81.74%), Space: 44.1 MB (60.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,5 +4,18 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return s.split("").sort().join("") === t.split("").sort().join("");
+    if(s.length !== t.length) return false;
+    const map = new Map();
+    for(const c of s){
+        if(map.has(c))
+                    map.set(c,map.get(c)+1);
+        else map.set(c,1);
+    }
+    for(const c of t){
+        if(!map.has(c)) return false;
+        map.set(c,map.get(c)-1);
+        if(map.get(c)===0) map.delete(c);
+    }
+    if(map.size) return false;
+    else return true;
 };
