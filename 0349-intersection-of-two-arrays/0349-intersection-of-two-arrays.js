f9bc7e830239f62546a9424a5761dc4e842f444a
harshita-priya1 <88089460+harshita-priya1@users.noreply.github.com>
2023-08-18 23:09:37 +0530

    Time: 66 ms (31.16%), Space: 43.8 MB (30.53%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -4,15 +4,19 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    let s = new Set();
-    let res=[];
-    for(let i of nums1){
-        s.add(i);
+    let map1 = new Map();
+    for(let num of nums1){
+        if(!map1.has(num))
+            map1.set(num, 1);
     }
-        for (let i of nums2)
-            if (s.has(i)) {
-                res.push(i);
-                s.delete(i);
-            }
-        return res;
+    let map2 = new Map();
+    for(let num of nums2){
+        if(!map2.has(num))
+            map2.set(num, 1);
+    }
+    let res = [];
+    map1.forEach((e,i)=>{
+        if(map2.has(i)) res.push(i);
+    })
+    return res;
 };
