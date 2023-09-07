b51a70b1d01ddca781c3e84821b5add603339be5
harshita-priya1 <88089460+harshita-priya1@users.noreply.github.com>
2023-09-07 19:22:07 +0530

    Time: 60 ms (52.46%), Space: 44.4 MB (10.73%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
@@ -5,17 +5,17 @@
  */
 var countPairs = function(nums, target) {
     nums.sort((a,b)=> a-b);
-    let cnt = 0;
+    let count = 0;
     let l = 0;
     let r = nums.length-1;
     while(l<r){
         if(nums[l] + nums[r] < target){
-            cnt += r-l;
+            count += r-l;
             l++;
         }
         else{
             r--;
         }
     }
-    return cnt;
+    return count;
 };
